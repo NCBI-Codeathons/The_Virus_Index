f59901fc089b47cb17011c4e5a8ea8adde41d971
Christiam Camacho <christiam@hotmail.com>
2019-11-06 07:26:53 -0500

    Minor

--- tests/check-module.sh
+++ tests/check-module.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # tests/check-module.sh: Script to check the module's deployment to PyPI
 #
-# Author: Christiam Camacho (christiam.camacho@gmail.com)
+# Author: Christiam Camacho (camacho@ncbi.nlm.nih.gov)
 # Created: Wed Nov  6 06:57:16 2019
 
 export PATH=/bin:/usr/bin:/usr/local/bin
